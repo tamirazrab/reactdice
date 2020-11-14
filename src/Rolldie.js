c172c85ba0308357ae665dbd81ad2572c91b0686
Tamir Azrab <tamirazrab@gmail.com>
2020-11-14 19:50:45 +0500

    Added: Styling of componenets

--- src/Rolldie.js
+++ src/Rolldie.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Die from "./Die";
+import "./Rolldice.css";
 
 class RollDice extends Component {
   static defaultProps = {
@@ -10,24 +11,28 @@ class RollDice extends Component {
     super(props);
     this.state = { firstDice: "one", secondDice: "one" };
   }
-  
+
   rollDice = () => {
-      const properties = this.props;
-      const sidesLenght = properties.sides.length;
-      let newFirstRolledDice = properties.sides[ Math.floor(Math.random() * sidesLenght)];
-      let newSecondRolledDice = properties.sides[ Math.floor(Math.random() * sidesLenght)];
+    const properties = this.props;
+    const sidesLenght = properties.sides.length;
+    let newFirstRolledDice =
+      properties.sides[Math.floor(Math.random() * sidesLenght)];
+    let newSecondRolledDice =
+      properties.sides[Math.floor(Math.random() * sidesLenght)];
 
-      
-
-      // updating dice states
-      this.setState({firstDice: newFirstRolledDice, secondDice: newSecondRolledDice});
-    }
+    // updating dice states
+    this.setState({
+      firstDice: newFirstRolledDice,
+      secondDice: newSecondRolledDice,
+    });
+  };
   render() {
     return (
-      <div>
-        <Die faceValue={this.state.firstDice} />
-        <Die faceValue={this.state.secondDice} />
-        
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Die faceValue={this.state.firstDice} />
+          <Die faceValue={this.state.secondDice} />
+        </div>
         <button onClick={this.rollDice}>Roll Dice</button>
       </div>
     );
